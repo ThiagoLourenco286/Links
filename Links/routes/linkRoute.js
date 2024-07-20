5e196f8e2414da254ee4cefa5c68fce36bb99622
Thiago Lourenço <Thiago.lourencooliveira12@gmail.com>
2024-07-20 11:59:03 -0300

    Commit inicial

---
+++ Links/routes/linkRoute.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const methodOverride = require('method-override');
+
+router.use(methodOverride('_method'));
+const linkController = require('../controlles/linkController');
+
+router.get('/', linkController.allLinks);
+router.get('/:title', linkController.redirect);
+router.get('/add', (req, res) => res.render('index', { error: false, body: {} }));
+router.get('/edit/:id', linkController.loadLink)
+
+router.post('/', express.urlencoded({ extended: true }), linkController.addLink)
+router.post('/edit/:id', express.urlencoded({ extended: true }), linkController.editLink)
+
+router.delete('/:id', linkController.deleteLink);
+router.delete('/', express.urlencoded({ extended: true }), linkController.deleteLink)
+
+module.exports = router;
